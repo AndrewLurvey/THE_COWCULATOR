24450c688118b2f127d8d614ac39c69707483124
AndrewLurvey <69861522+AndrewLurvey@users.noreply.github.com>
2020-08-18 09:13:19 -0700

    Add files via upload

--- cow.py
+++ cow.py
@@ -1,17 +1,19 @@
 import math
+print("Please only use numbers or the program will crash")
+
 x = int(input("How many years"))
 cow_to_add_total = 0
 y = 0
 
-cow_sell_price = 800
-cow_buy_price = 1200
-cow_keep_price = 210
-
+starting_cows = int(input("How many cows are you starting with?"))#6
+cow_sell_price = int(input("How much are the calves going for? (800) "))#800
+cow_buy_price = int(input("How much does a calving cow cost? (1200) "))#1200
+cow_keep_price = int(input("How much does it cost to keep the a cow (210) "))#210
 
 while y != x:
-    cows = 6 + cow_to_add_total
+    cows = starting_cows + cow_to_add_total
     
-    print(str(y))
+    print(str(y + 1))
     print(" Number of Cows: " + str(cows))
     print("")
     print(" Cow cost: " + str(cows * cow_keep_price))
@@ -25,3 +27,7 @@ while y != x:
     print(" Cows bought this generation: " + str(cow_to_add_this_gen))
 
     y += 1
+
+while 1:
+    if input("Press any key then enter to end the program"):
+        break
