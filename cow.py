4386265afe30fc944dfe8eda100f8534f9a6fe70
AndrewLurvey <69861522+AndrewLurvey@users.noreply.github.com>
2020-08-18 09:38:38 -0700

    CAPS
    Fixed some capitalization mistakes

--- cow.py
+++ cow.py
@@ -5,10 +5,10 @@ x = int(input("How many years"))
 cow_to_add_total = 0
 y = 0
 
-starting_cows = int(input("How many cows are you starting with?"))#6
+starting_cows = int(input("How many cows are you starting with? (6)"))#6
 cow_sell_price = int(input("How much are the calves going for? (800) "))#800
 cow_buy_price = int(input("How much does a calving cow cost? (1200) "))#1200
-cow_keep_price = int(input("How much does it cost to keep the a cow (210) "))#210
+cow_keep_price = int(input("How much does it cost to keep the a cow? (210) "))#210
 
 while y != x:
     cows = starting_cows + cow_to_add_total
@@ -17,9 +17,9 @@ while y != x:
     print(" Number of Cows: " + str(cows))
     print("")
     print(" Cow cost: " + str(cows * cow_keep_price))
-    print(" cow profit: " + str(cows * cow_sell_price))
+    print(" Cow profit: " + str(cows * cow_sell_price))
     left_over = (cows * cow_sell_price) - (cows * cow_keep_price)
-    print(" cow break even: " + str(left_over))
+    print(" Cow break even: " + str(left_over))
     print("")
     cow_to_add_total += math.floor(left_over / cow_buy_price)
     cow_to_add_this_gen = math.floor(left_over / cow_buy_price)
